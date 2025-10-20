b28fbeb257ca4ec5477a1fca2e9bb662469fe3d1
Sham Ganesh Thamarai Kannan <stha0083@student.monash.edu>
2025-10-20 15:57:18 +0800

    Create the basic UI layout of the profile form for the remote job matcher

--- pages/profile.py
+++ pages/profile.py
@@ -6,3 +6,20 @@ st.set_page_config(
     page_icon="👤",
     layout="centered"
 )
+
+# Title of the page
+st.title("Set Up Your Profile")
+
+# Instructions
+st.write("Please fill up the details below to set up your profile start using the Job Matcher:")
+
+# Signup form
+with st.form(key='profile_form'):
+
+    username = st.text_input("Username", key='profile_username')
+    email = st.text_input("Email", key='profile_email')
+
+    # Submit button
+    save_button = st.form_submit_button(
+        label="Save Profile"
+    )

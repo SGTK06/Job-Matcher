01321126523a102ccb0b0b6a7566d59fb18762ef
Sham Ganesh Thamarai Kannan <stha0083@student.monash.edu>
2025-10-20 16:37:18 +0800

    setup data manager files to parse data stored in csv files

---
+++ src/data_manager.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from config import *
+
+class DataManager:
+    """
+    class to read csv data and manage the user data in csv files
+    """
+    def __init__(self):
+        pass

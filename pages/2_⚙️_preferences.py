ea73d84484d37d3e8d40dcb54482d4e8a1668563
Sham Ganesh Thamarai Kannan <stha0083@student.monash.edu>
2025-10-20 18:21:04 +0800

    set up the preferences page for next set of inputs and change the page icon of progile page

---
+++ pages/2_⚙️_preferences.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+# Set page configuration
+st.set_page_config(
+    page_title="Prferences Page",
+    page_icon="⚙️",
+    layout="centered"
+)
+
+# Title of the page
+st.title("Set Up Your Preferences")
+
+# Instructions
+st.write("Please enter your skills and preferences in the form below:")

89444753d073bfc226c2a4f97f6ecc50b5493694
Sham Ganesh Thamarai Kannan <stha0083@student.monash.edu>
2025-10-20 16:24:31 +0800

    write testcases to check the mail validation

--- tests/test_input_validation.py
+++ tests/test_input_validation.py
@@ -4,7 +4,7 @@ for: FIT2107 D1 - Custom Project - Job Matcher
 """
 
 import unittest
-from src.user_input_validation import validate_user_name
+from src.user_input_validation import validate_user_name, validate_email
 
 class TestInputValidation(unittest.TestCase):
     """class to test the validation logic og the user input"""
@@ -40,3 +40,33 @@ class TestInputValidation(unittest.TestCase):
         """test case to check if space user name input is rejected by the validation logic"""
         empty_name = "        "
         self.assertFalse(validate_user_name(empty_name))
+
+    def test_empty_mail(self):
+        """test case to check if the empty mail id is rejected"""
+        empty_mail_id = ""
+        self.assertFalse(validate_email(empty_mail_id))
+
+    def test_no_separator_mail(self):
+        """test case to check if the no separator @ mail id is rejected"""
+        empty_mail_id = "maildomain.com"
+        self.assertFalse(validate_email(empty_mail_id))
+
+    def test_no_id_mail(self):
+        """test case to check if the no address mail id is rejected"""
+        empty_mail_id = "@domain.com"
+        self.assertFalse(validate_email(empty_mail_id))
+
+    def test_no_domain_mail(self):
+        """test case to check if the no domain mail id is rejected"""
+        empty_mail_id = "mail@.com"
+        self.assertFalse(validate_email(empty_mail_id))
+
+    def test_no_extension_mail(self):
+        """test case to check if the no extension mail id is rejected"""
+        empty_mail_id = "mail@domain"
+        self.assertFalse(validate_email(empty_mail_id))
+
+    def test_valid_mail(self):
+        """test case to check if the valid mail id is accepted"""
+        empty_mail_id = "mail@domain.extension"
+        self.assertTrue(validate_email(empty_mail_id))

827597d0eea3a8e7cd05d878a329747a70999a57
Sham Ganesh Thamarai Kannan <stha0083@student.monash.edu>
2025-10-20 18:34:30 +0800

    write testcases to check the skills input string validation logic

--- tests/test_input_validation.py
+++ tests/test_input_validation.py
@@ -4,7 +4,7 @@ for: FIT2107 D1 - Custom Project - Job Matcher
 """
 
 import unittest
-from src.user_input_validation import validate_user_name, validate_email
+from src.user_input_validation import *
 
 class TestInputValidation(unittest.TestCase):
     """class to test the validation logic og the user input"""
@@ -70,3 +70,33 @@ class TestInputValidation(unittest.TestCase):
         """test case to check if the valid mail id is accepted"""
         empty_mail_id = "mail@domain.extension"
         self.assertTrue(validate_email(empty_mail_id))
+
+    def test_less_skills(self):
+        """test case to check if a low number of skills input is rejected
+        eqv partiiton"""
+        skill_string = "skill1, skill2"
+        self.assertFalse(validate_skills(skill_string))
+
+    def test_4_skills(self):
+        """test case to check if input of 4 skills input is rejected
+        BVA"""
+        skill_string = "skill1, skill2, skill3, skill4"
+        self.assertFalse(validate_skills(skill_string))
+
+    def test_appropriate_skills(self):
+        """test case to check if input of 5 skills input is accepted
+        BVA"""
+        skill_string = "skill1, skill2, skill3, skill4, skill5"
+        self.assertTrue(validate_skills(skill_string))
+
+    def test_one_extra_skills(self):
+        """test case to check if input of 6 skills input is accepted
+        BVA"""
+        skill_string = "skill1, skill2, skill3, skill4, skill5, skill6"
+        self.assertTrue(validate_skills(skill_string))
+
+    def test_large_number_of_skills(self):
+        """test case to check if a lot of skills input is accepted
+        eqv part"""
+        skill_string = "skill1, skill2, skill3, skill4, skill5, skill6, s7, s8, s9, s10, s11, s12, s13. s14"
+        self.assertTrue(validate_skills(skill_string))

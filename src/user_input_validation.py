ac234183597658fcc8cab6f3a74257443f6fe531
Sham Ganesh Thamarai Kannan <stha0083@student.monash.edu>
2025-10-20 16:04:17 +0800

    Create method signature of the testcase of username validation function

---
+++ src/user_input_validation.py
@@ -0,0 +1,10 @@
+def validate_user_name(user_name):
+    """
+    Function to check if the given user name input is valid.
+    User name is considered valid if:
+        - user name is not empty
+        - user name is longer than 5 characters
+    If valid -> returns True
+    else     -> returns Flase
+    """
+    return True

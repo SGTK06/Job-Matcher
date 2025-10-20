de331bee57493f41da57f72fef1d3a659382a324
Sham Ganesh Thamarai Kannan <stha0083@student.monash.edu>
2025-10-20 16:48:03 +0800

    implement data handler functions to pass tests

--- src/data_manager.py
+++ src/data_manager.py
@@ -1,12 +1,12 @@
 import pandas as pd
-from src import config
+from src.config import *
 
 class DataManager:
     """
     class to read csv data and manage the user data in csv files
     """
     def __init__(self):
-        pass
+        self.user_data_frame = pd.read_csv(USER_DATA)
 
     def is_signed_in(self):
-        pass
+        return self.user_data_frame.empty

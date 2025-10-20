997ae334f64796518e801f526c1bf4a9d85601f4
Sham Ganesh Thamarai Kannan <stha0083@student.monash.edu>
2025-10-20 18:01:59 +0800

    use the previously created fucntions to make the sign up page functional and accept and save data

--- pages/profile.py
+++ pages/profile.py
@@ -1,3 +1,8 @@
+import sys
+import os
+#to avoid streamlit import path errors => source:https://stackoverflow.com/questions/68033795/avoiding-sys-path-append-for-imports
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import streamlit as st
 
 from src.user_input_validation import *
@@ -16,11 +21,17 @@ st.title("Set Up Your Profile")
 # Instructions
 st.write("Please fill up the details below to set up your profile start using the Job Matcher:")
 
+data_manager = DataManager()
 # Signup form
 with st.form(key='profile_form'):
+    if data_manager.is_signed_in():
+        user_data = data_manager.get_user_data()
+        username = st.text_input("Username", key='profile_username', placeholder="5+ chars", value=user_data["user_name"])
+        email = st.text_input("Email", key='profile_email', placeholder="mail@domain.extension", value=user_data["user_mail"])
 
-    username = st.text_input("Username", key='profile_username', placeholder="5+ chars")
-    email = st.text_input("Email", key='profile_email', placeholder="mail@domain.extension")
+    else:
+        username = st.text_input("Username", key='profile_username', placeholder="5+ chars")
+        email = st.text_input("Email", key='profile_email', placeholder="mail@domain.extension")
 
     # Submit button
     save_button = st.form_submit_button(
@@ -35,7 +46,6 @@ if save_button:
         st.error("Invalid mail address, please check your mail id!")
 
     else:
-        data_manager = DataManager()
         data_manager.register_user(username, email)
         saved = data_manager.save_user()
         if saved:

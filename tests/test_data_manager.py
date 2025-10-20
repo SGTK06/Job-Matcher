022225ad91a0bf7c019f39d1bf07570833b33483
Sham Ganesh Thamarai Kannan <stha0083@student.monash.edu>
2025-10-20 16:57:21 +0800

    test signed in check

--- tests/test_data_manager.py
+++ tests/test_data_manager.py
@@ -1,5 +1,6 @@
 import unittest
 import pandas
+from unittest import mock
 
 from src.data_manager import DataManager
 
@@ -15,3 +16,15 @@ class TestDataManager(unittest.TestCase):
     def test_signed_in(self):
         new_manager = DataManager()
         self.assertFalse(new_manager.is_signed_in())
+
+    @mock.patch("pandas.read_csv")
+    def test_signed_in_after_loading_data(self, user_df):
+        # initialize data of lists.
+        data = {"user_name": ["Peter"],
+                "user_mail": ["parker@marvel.com"]}
+
+        # Create DataFrame
+        user_df.return_value = pandas.DataFrame(data)
+
+        new_manager = DataManager()
+        self.assertTrue(new_manager.is_signed_in())

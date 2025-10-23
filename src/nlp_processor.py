10eccbae18b6845707946bda98a262d3532e0adb
Sham Ganesh Thamarai Kannan <stha0083@student.monash.edu>
2025-10-24 01:25:40 +0800

    create method signatures for the NLP processor class creation and tokenizer

---
+++ src/nlp_processor.py
@@ -0,0 +1,11 @@
+import spacy
+
+class NlpProcessor():
+    """Handles the NLP processing data pipeline for comparing keywords
+    of job description and applicatn skills"""
+
+    def __init__(self):
+        """constructor for processor"""
+
+    def tokenizer(self, keyword_list):
+        """takes a list of key words and converts them into tokens for processing"""

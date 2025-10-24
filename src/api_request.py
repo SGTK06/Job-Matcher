5d834616cb60a5c301e9d798fc0c033ed322a97e
Sham Ganesh Thamarai Kannan <stha0083@student.monash.edu>
2025-10-24 10:43:01 +0800

    create user agents and proxies helper functions to query API and also implement more stable method for fallback

--- src/api_request.py
+++ src/api_request.py
@@ -1,4 +1,6 @@
 import requests
+from fake_useragent import UserAgent
+from src.config import PROXY_NAME, PROXY_AUTH
 
 class ApiRequest:
     """
@@ -7,6 +9,21 @@ class ApiRequest:
     """
     def __init__(self):
         """class to construct call handler"""
+        self.access_headers =  {
+            "User-Agent" : UserAgent().random,
+            "Accept-Language" : "en-US,en;q=0.9",
+            "Connection" : "keep-alive",
+            "Referer" : "https://www.google.com"
+        }
+        self.rotating_proxy = self.create_rotating_proxy(PROXY_NAME, PROXY_AUTH)
+        self.request_session = requests.Session()
+
+    def create_rotating_proxy(self, proxy_name, proxy_auth):
+        rotating_proxies = {
+            "http": f"http://{proxy_name}:{proxy_auth}@p.webshare.io:80/",
+            "https": f"http://{proxy_name}:{proxy_auth}@p.webshare.io:80/"
+        }
+        return rotating_proxies
 
     def get_request(self ,url, query_limit):
         """
@@ -18,3 +35,22 @@ class ApiRequest:
         returns:
          - response : in json format
          """
+        if query_limit is not None:
+            url = url + f"?limit={query_limit}"
+
+        response = self.request_session.get(
+            url,
+            headers=self.access_headers,
+            proxies=self.rotating_proxy
+        )
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            #fallback to normal query
+            response = self.request_session.get(url)
+            if response.status_code == 200:
+                return response.json()
+            else:
+                print("ERROR FETCHING DATA USING API")
+                return {}
--- src/config.py
+++ src/config.py
@@ -1,3 +1,7 @@
 USER_DATA = "data/user_data.csv"
 USER_PREFERENCES = "data/user_preferences.csv"
 REMOTIVE_API = "https://remotive.com/api/remote-jobs"
+#created dedicated free webshare account for FIT2107 to immediately test the proxy
+#and query API without need for prerequisite account creation
+PROXY_NAME="smcujzjp-rotate"
+PROXY_AUTH="jz0gg0kvo06l"

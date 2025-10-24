7c84b663392821a111a4a8f407e61cca91975045
Sham Ganesh Thamarai Kannan <stha0083@student.monash.edu>
2025-10-24 11:03:58 +0800

    create method signature to evaluate job suitability

---
+++ src/job_matching_logic.py
@@ -0,0 +1,9 @@
+from data_manager import DataManager
+from nlp_processor import NlpProcessor
+
+def evaluate_job(job_details):
+    """
+    Function to evalueate a job's suitability
+     - if suitable, True
+     - else, False
+    """

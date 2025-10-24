6ac831dfc5b8617ca7498b5bf416c9c538517413
Sham Ganesh Thamarai Kannan <stha0083@student.monash.edu>
2025-10-24 08:45:45 +0800

    create the processing function to handle nlp pipeline and calcuate the match percentage

--- src/nlp_processor.py
+++ src/nlp_processor.py
@@ -17,27 +17,55 @@ class NlpProcessor():
             tokens_list.append(tokens)
         return tokens_list
 
-    def token_comparison(self, sourceTokens, targetTokens):
+    def token_comparison(self, source_tokens, target_tokens):
         """
         For every token in a list of tokens, find the best matching token
         in the other list and save the tokens details for comparison
         """
         comparison = []
         #check for every token in source keyword tokens
-        for sourceToken in sourceTokens:
+        for source_token in source_tokens:
 
-            bestMatchedToken = None
-            bestMatchedScore = 0
+            best_matched_token = None
+            best_matched_score = 0
             #find best match in target tokens
-            for targetToken in targetTokens:
-                tokenScore = sourceToken.similarity(targetToken)
+            for target_token in target_tokens:
+                token_score = source_token.similarity(target_token)
 
-                if tokenScore > bestMatchedScore:
-                    bestMatchedScore = tokenScore
-                    bestMatchedToken = targetToken
+                if token_score > best_matched_score:
+                    bestMatchedScore = token_score
+                    bestMatchedToken = target_token
 
-            comparison.append({"sourceToken":sourceToken, "matchedToken":bestMatchedToken, "score":bestMatchedScore})
+            comparison.append({"sourceToken":source_token, "matchedToken":best_matched_token, "score":best_matched_score})
         return comparison
+
+    def comparison_score(self, token_comparison):
+        """
+        Function to calculate the match percentage of source
+        and target keywords based on threshold value of comparison
+        and return number of matched keywords as %
+        """
+        match = 0
+        #find number of keywords that match more than the threshold
+        #value and get as %
+        for comparison in token_comparison:
+            if comparison["score"] > self.comparisonThreshold:
+                match += 1
+
+        if len(token_comparison) > 0:
+            match_percentage = match/len(token_comparison) * 100
+        else:
+            match_percentage = 0
+
+        return match_percentage
 
     def compare_keywords(self, source_keyword_list, target_keyword_list):
         """compares keywords in source keyword list and target keyword list"""
+        source_tokens = self.tokenizer(source_keyword_list)
+        target_tokens = self.tokenizer(target_keyword_list)
+        #print(target_tokens, source_tokens)
+
+        token_comparison = self.token_comparison(source_tokens, target_tokens)
+
+        comparison_score = self.comparison_score(token_comparison)
+        return comparison_score

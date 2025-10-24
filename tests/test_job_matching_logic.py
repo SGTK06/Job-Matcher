6c521f98e1ef039c3b3bc6f7a6242e69753e75b2
Sham Ganesh Thamarai Kannan <stha0083@student.monash.edu>
2025-10-24 13:28:23 +0800

    write category partition black box tests to check if the job suitability is evaluated correctly in all 4 cases

---
+++ tests/test_job_matching_logic.py
@@ -0,0 +1,173 @@
+"""
+File for testing the job matcher logic
+author: Sham Ganesh Thamarai Kannan
+"""
+
+import unittest
+from unittest import mock
+import pandas
+
+from src.job_matching_logic import evaluate_job
+from src.data_manager import DataManager
+from src.config import USER_DATA, USER_PREFERENCES
+
+def get_user_data(self):
+    return {
+        "user_name": "Ash Ketchum",
+        "user_mail": "ashk@pokemon.com"
+    }
+
+def get_preferences(self):
+    return {
+        "user_skills": [
+            "python",
+            "numpy",
+            "pandas",
+            "testing",
+            "data",
+            "excel",
+            "algorithms",
+            "agile",
+            "java",
+            "math"],
+        "min_salary": 1
+        }
+
+class TestJobMatching(unittest.TestCase):
+    """
+    Class to test the job matching logic of the system
+    """
+    def set_up_testing_env(self):
+        data_manager = DataManager()
+        #make deep copy to prevent mutation of user data during testing
+        self._production_user_data = data_manager.user_data_frame.copy(deep=True)
+        self._production_user_pref = data_manager.user_preferences.copy(deep=True)
+
+        #keep headers only
+        user_data_headers = pandas.read_csv(USER_DATA, nrows=0)
+        user_pref_headers = pandas.read_csv(USER_PREFERENCES, nrows=0)
+        user_data_headers.to_csv(USER_DATA, index=False)
+        user_pref_headers.to_csv(USER_PREFERENCES, index=False)
+
+    def restore_production_env(self):
+        self._production_user_data.to_csv(USER_DATA, index=False)
+        self._production_user_pref.to_csv(USER_PREFERENCES, index=False)
+
+
+    def setUp(self):
+        self.user_skills = \
+            "python,\
+            numpy,\
+            pandas,\
+            testing,\
+            data,\
+            excel,\
+            algorithms,\
+            agile,\
+            java,\
+            math"
+
+        self.pref_salary = "1" #  k (1000)
+
+        self.suitable_tags = [
+            "python",
+            "numpy",
+            "pandas",
+            "testing",
+            "data",
+            "excel",
+            "algorithms",
+            "agile",
+            "java",
+            "math"
+        ]
+
+        self.unsuitable_tags = [
+            "project management",
+            "physics",
+            "chemistry",
+            "genetics",
+            "biology",
+            "mechanical",
+            "robotics"
+        ]
+
+        self.suitable_salary = "2"
+        self.unsuitable_salary = "0"
+
+
+
+    def test_different_skills_unsuitable_salary(self):
+        self.set_up_testing_env()
+
+        data_manager = DataManager()
+        data_manager.register_user("userX", "ux@mail.com")
+        data_manager.register_preferences(
+            self.user_skills,
+            self.pref_salary
+        )
+
+        job_details = {
+            "salary": self.unsuitable_salary,
+            "tags": self.unsuitable_tags
+        }
+
+        suitability = evaluate_job(job_details)
+        self.assertFalse(suitability)
+
+    def test_different_skills_suitable_salary(self):
+
+        data_manager = DataManager()
+        data_manager.register_user("userX", "ux@mail.com")
+        data_manager.register_preferences(
+            self.user_skills,
+            self.pref_salary
+        )
+
+        job_details = {
+            "salary": self.suitable_salary,
+            "tags": self.unsuitable_tags
+        }
+
+        suitability = evaluate_job(job_details)
+        self.assertFalse(suitability)
+
+    def test_matched_skills_unsuitable_salary(self):
+        data_manager = DataManager()
+        data_manager.register_user("userX", "ux@mail.com")
+        data_manager.register_preferences(
+            self.user_skills,
+            self.pref_salary
+        )
+
+        job_details = {
+            "salary": self.unsuitable_salary,
+            "tags": self.suitable_tags
+        }
+
+        suitability = evaluate_job(job_details)
+        self.assertFalse(suitability)
+
+    def test_matched_skills_suitable_salary(self):
+        data_manager = DataManager()
+        data_manager.register_user("userX", "ux@mail.com")
+        data_manager.register_preferences(
+            self.user_skills,
+            self.pref_salary
+        )
+
+        job_details = {
+            "salary": self.suitable_salary,
+            "tags": self.suitable_tags
+        }
+
+        suitability = evaluate_job(job_details)
+        self.assertTrue(suitability)
+
+        self.restore_production_env()
+
+
+    """
+    @mock.patch("src.data_manager.DataManager.get_user_data", side_effect = get_user_data)
+    @mock.patch("src.data_manager.DataManager.get_preferences", side_effect = get_preferences)
+    """

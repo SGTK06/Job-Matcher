39e93731dfa285cb50bff2483642579f14a8e6ef
Sham Ganesh Thamarai Kannan <stha0083@student.monash.edu>
2025-10-24 08:34:06 +0800

    create helper function tokenizer to convert keywrods to tokens

--- src/nlp_processor.py
+++ src/nlp_processor.py
@@ -6,6 +6,16 @@ class NlpProcessor():
 
     def __init__(self, match_percentage):
         """constructor for processor"""
+        self.nlp = spacy.load("en_core_web_md")
+        self.comparisonThreshold = match_percentage
+
+    def tokenizer(self, keywordList):
+        tokensList = []
+        #split the given list of keywords to tokens to be compared
+        for keyword in keywordList:
+            tokens = self.nlp(keyword)[0]
+            tokensList.append(tokens)
+        return tokensList
 
     def compare_keywords(self, source_keyword_list, target_keyword_list):
         """compares keywords in source keyword list and target keyword list"""

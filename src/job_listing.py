5b7c15f4559541fbdd7777efd37ce25b6276fd17
Sham Ganesh Thamarai Kannan <stha0083@student.monash.edu>
2025-10-26 18:01:54 +0800

    create constructor for job listing object to process data to correct format

--- src/job_listing.py
+++ src/job_listing.py
@@ -6,13 +6,47 @@ display
 author: Sham Ganesh Thamarai Kannan
 for: FIT2107 D2
 """
+from datetime import datetime
+
 class JobListing:
     """
     class that represents a job listings and processes
     the job details returned by the API to store them
     in the correct info format
     """
-    def __init__(self, job_details):
+    def __init__(self, job_data):
         """
         constructor function to create a job object/instance
         """
+
+        self.id = int(job_data["id"])
+        self.url = job_data["url"]
+        self.title = job_data["title"]
+        self.company_name = job_data["company_name"]
+        self.company_logo =  job_data["company_logo"]
+        self.category = job_data["category"]
+        self.req_skills = job_data["tags"]
+        self.job_type = job_data["job_type"]
+
+        try:
+            self.publication_date = datetime.fromisoformat(
+                job_data["publication_date"]
+            )
+        except ValueError:
+            self.publication_date = job_data["publication_date"]
+        except TypeError:
+            self.publication_date = job_data["publication_date"]
+
+        self.location = job_data["candidate_required_location"]
+
+        try:
+            self.salary = float(job_data["salary"])
+        except ValueError:
+            self.salary = job_data["salary"]
+
+        try:
+            self.description = job_data["description"].split(r"</p>")[0].strip(r"<p>")
+        except TypeError:
+            self.desciption = job_data["description"]
+
+        self.application_status = False

8bb7d5cc0184146d3e6a94cc82334e323d3e9c0b
Sham Ganesh Thamarai Kannan <stha0083@student.monash.edu>
2025-11-01 15:35:13 +0800

    correct testdocs

--- tests/test_api_request.py
+++ tests/test_api_request.py
@@ -15,19 +15,24 @@ and API caller functions as expected.
 Blackbox Tests:
 Expected:
 1. Returns a response in JSON format (python dictionary): https://github.com/remotive-com/remote-jobs-api
-2. Return empty dictionary on connection timeout
-3. Return empty dictionary on facing network issues
-4. Return empty dictionary on requesting response from fake API/URL
+2. Return empty dictionary on requesting response from fake API/URL
+
+Whitebox Tests:
+Expected:
+1. Return empty dictionary on connection timeout
+2. Return empty dictionary on facing network issues
 """
 class TestApiRequest(unittest.TestCase):
     """
     class to test if the API caller works as expected
     """
-    def test_response_format(self):
+    def setUp(self):
+        self.caller = ApiRequest()
+
+    def test_response_format_bt1(self):
         """
         tests if the caller returns a dictionary (
         json format in python)
         """
-        caller = ApiRequest()
-        resp = caller.get_request(REMOTIVE_API, 5)
+        resp = self.caller.get_request(REMOTIVE_API, 5)
         self.assertEqual(type(resp), dict)

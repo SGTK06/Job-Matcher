b0741fe671eb435c76d97483506eb7567c42a1e1
Sham Ganesh Thamarai Kannan <stha0083@student.monash.edu>
2025-11-01 23:30:09 +0800

    write documentation for black box testing of naive NLP processor

--- tests/test_nlp_processor.py
+++ tests/test_nlp_processor.py
@@ -1,7 +1,32 @@
+"""
+File to test the functions that handle word matching
+using Natural Language Processing.
+author: Sham Ganesh Thamarai Kannan
+for: FIT2107 D1 - Custom Project - Job Matcher
+"""
+
 import unittest
 
 from src.nlp_processor import NlpProcessor
 import spacy
+
+"""
+Tests whether the NLP Processor matches words with similar meaning
+correctly, and handles edge cases effectively.
+
+Blackbox Tests:
+Expected:
+BT01. comparison of empty lists of keywords returns score of 0
+BT02. comparison with/against an empty list also returns score 0
+BT03. comparison of same words in different tense should result
+      high score
+BT04. comparison of same words in different case should result in
+      in a score of 100%
+BT05. comparison of same words surrounded by different puntuation
+      should result in a score of 100%
+BT06. comparison of same words should result in a score of 100%
+"""
+
 
 class TestNlpProcessor(unittest.TestCase):
     """class to test the functionality of NLP processing pipeline"""

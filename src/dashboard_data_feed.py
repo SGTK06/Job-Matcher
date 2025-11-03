d5ef25542e323b755e7facdcf9a47520e602941b
Sham Ganesh Thamarai Kannan <stha0083@student.monash.edu>
2025-11-04 03:52:10 +0800

    create data feed

--- src/dashboard_data_feed.py
+++ src/dashboard_data_feed.py
@@ -1,8 +1,27 @@
 import pandas as pd
 
 from src.job_listing_manager import ListingManager
+from src.user_data_manager import DataManager
 
 class DashboardDataFeed:
     def __init__(self):
-        listing_manager = ListingManager()
-        all_jobs = listing_manager.get_listings()
+        self.user_data_manager = DataManager()
+        self.listing_manager = ListingManager()
+
+    def get_user_status(self):
+        """Get user sign-in and preferences status"""
+        return {
+            "is_signed_in": self.user_data_manager.is_signed_in(),
+            "has_preferences": self.user_data_manager.has_preferences()
+        }
+
+    def get_user_preferences(self):
+        """Get user preferences"""
+        return self.user_data_manager.get_preferences()
+
+    def get_data_feed(self):
+        """get the feed of jobs to display"""
+        data = []
+        job_records = self.listing_manager.get_listings()
+        return job_records
+
